9b036b241bc31cdb7c277f111c33efbae7cf372d
DenisMuga <denismugah5@gmail.com>
2022-03-29 16:45:52 +0300

    More JS lessons

--- scripts.js
+++ scripts.js
@@ -135,17 +135,153 @@
 // console.log(result); //Note that methods do not alter the original value of the string.
 
 
-let email = 'mugah2011@gmail.com';
-let index = email.indexOf('@'); //string method that gets the index of a character within a string
-let brii = email.lastIndexOf('a'); //logs the last index of a which is 12
-let mySlice = email.slice(0, 8); //gets the first 8 characters of the string
-let mySubst = email.substr(2,10); //picks 10 characters from position 2
-let myReplace = email.replace('m', 'w'); //Replaces the first m character in the string with w
-console.log(brii);
-console.log(index);
-console.log(mySlice);
-console.log(mySubst);
-console.log(myReplace);
+// let email = 'mugah2011@gmail.com';
+// let index = email.indexOf('@'); //string method that gets the index of a character within a string
+// let brii = email.lastIndexOf('a'); //logs the last index of a which is 12
+// let mySlice = email.slice(0, 8); //gets the first 8 characters of the string
+// let mySubst = email.substr(2,10); //picks 10 characters from position 2
+// let myReplace = email.replace('m', 'w'); //Replaces the first m character in the string with w
+// console.log(brii);
+// console.log(index);
+// console.log(mySlice);
+// console.log(mySubst);
+// console.log(myReplace);
+
+
+// ===================NUMBERS IN JS =========================================
+
+// const pi = 3.142;
+// let radius = 7;
+// let area = pi * radius**2;
+// console.log(area);
+
+// // let vehicles = 30
+// // vehicles = vehicles +1;
+// // console.log(vehicles);
+
+
+// //OR  
+
+// let vehicles = 31;
+// // vehicles++;
+// //vehicles+=10;
+// //vehicles-=10;
+// //vehicles *=3;
+// vehicles /=10;
+
+
+// console.log(vehicles);
+
+// // ===============concatenation way ======================================
+
+// let club = 'Arsenal';
+// let trophies = 20;
+// let word = 'between the year 2000 and 2022';
+
+// let combi =  club  + ' club' + ' has won ' + trophies + ' trophies ' + word;
+// console.log(combi);
+
+// //=========================template string way ======================================
+
+// let combin = `The club called ${club} has won ${trophies} trophies ${word}`; //Remember to use backticks `` to enclose the sentence.
+// console.log(combin);
+
+// // ============================Using HTML Templates ===================================
+
+// let html = `
+//     <h1> ${club}</h1>
+//     <p> has won ${trophies} trophies</p>
+//     <p> ${word}</p>
+// `
+
+// console.log(html);
+
+//=================================ARRAYS==============================================
+
+// let vehicles = ['benz', 'rangerover', 'nissan', 'mazda', 'premio']; //However, arrays can store different data types like strings and numbers
+// console.log(vehicles)
+// console.log(vehicles[0]);
+// vehicles[0] = 'mitsubishi';
+// console.log(vehicles);
+// console.log(vehicles[0]);
+
+// console.log(vehicles.length); //Counts the number of elements in the array = 5
+
+// //Array Methods
+
+// let joiner = vehicles.join(','); //Joins the array elements seperated by comas. You can change the seperating symbol by . / ? < > - _ + ' et al
+// let index = vehicles.indexOf('nissan'); //Finds the index of a particular array element
+// let myConcat = vehicles.concat(['wish', 'vitz', 'raum', 'passo', 'rav4', 'markx']); //Adds more elements into the array
+// let myPush = vehicles.push('rvr'); //Increases the length of the original array to 6. Console log to find 'rvr' at the end.
+// let myPop = vehicles.pop(); //Removes and logs the last value of the array = rvr
+// console.log(joiner);
+// console.log(index);
+// console.log(myConcat);
+// console.log(myPush);
+// console.log(vehicles);
+// console.log(myPop);
+// console.log(vehicles);
+
+//========================UNDEFINED================================
+
+// let age;
+// console.log(age, age + 10, `his age is ${age}`); //result: age is undefined, NaN, his age is undefined
+
+// // ==========================NULL =======================
+// let time = null;
+// console.log(time, time + 10, `his time is ${time}`); //result: null, 10, his time is null
+
+//=======================BOOLEAN==============================
+
+// let email = 'mugah2011@gmail.com';
+// let myCondition = email.includes('@');
+// console.log(myCondition); //Returns true
+
+
+// let myName = 'Denis Mugah'
+// let myIntent = myName.includes('z');
+// console.log(myIntent); //Returns false
+
+// let myArray = ['Denis', 'Muga', 'Okoth'];
+// let myFinding = myArray.includes('James');
+// console.log(myFinding); //Returns false
+
+
+//=======================COMPARISON OPERATOR ==============================================
+
+// let date = '29-03-2022';
+// console.log(date == '29-03-2022'); //Returns true - Remember to use == and not =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
